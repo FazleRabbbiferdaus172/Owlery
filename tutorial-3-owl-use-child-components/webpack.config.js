94896fc4b142604ef7a075838d1b652a7bbdf0dd
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-30 14:21:47 -0500

    title of index changed

--- tutorial-3-owl-use-child-components/webpack.config.js
+++ tutorial-3-owl-use-child-components/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'tutorial-2-css-library-setup-webpack-owl',
+            title: 'tutorial-3-owl-use-child-components',
         }),
     ]
 }
